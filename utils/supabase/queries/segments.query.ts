289f22ea8951f85bf1dd793f35d8718f8269f14e
tonymtz <atony.mtz@gmail.com>
2025-07-03 18:58:06 -0700

    weekly activity

--- utils/supabase/queries/segments.query.ts
+++ utils/supabase/queries/segments.query.ts
@@ -1,4 +1,5 @@
 import { SupabaseClient, User } from '@supabase/supabase-js'
+import { addDays, startOfWeek } from 'date-fns'
 
 import { SegmentType } from '~/utils/config'
 
@@ -20,4 +21,37 @@ export async function createSegment (context: SupabaseContext, type: SegmentType
   if (error) {
     throw error
   }
+}
+
+export type WeelyReport = Array<{ day: string, segments: any[] }>
+
+export async function retrieveWeeklyReport (context: SupabaseContext) {
+  if (!context.user) {
+    return
+  }
+
+  const from = startOfWeek(new Date(), { weekStartsOn: 1 }) // monday
+  const to = addDays(from, 7)
+
+  const { data, error } = await context.supabase
+    .from('segments')
+    .select('*')
+    .gte('created_at', from.toISOString())
+    .lt('created_at', to.toISOString())
+
+  const grouped = Array.from({ length: 7 }, (_, i) => {
+    const day = addDays(from, i).toISOString().slice(0, 10)
+    return {
+      day,
+      segments: data?.filter(seg =>
+        seg.created_at.startsWith(day)
+      ) || [],
+    }
+  })
+
+  if (error) {
+    throw error
+  }
+
+  return grouped satisfies WeelyReport
 }
