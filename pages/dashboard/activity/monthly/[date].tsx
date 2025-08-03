43b99eca63825ec00313fee77fa329e9bcdab4c5
tonymtz <atony.mtz@gmail.com>
2025-08-02 19:53:22 -0700

    monthly streak chart

--- pages/dashboard/activity/monthly/[date].tsx
+++ pages/dashboard/activity/monthly/[date].tsx
@@ -4,13 +4,13 @@ import { router } from 'next/client'
 import useTranslation from 'next-translate/useTranslation'
 import React, { useCallback } from 'react'
 import DatePicker from 'react-datepicker'
+import { Card, Flex, Heading } from 'theme-ui'
 
+import { FocusStreakChart } from '~/components/organisms/charts/monthly/focus-streak-chart.component'
 import { DashboardPage } from '~/components/templates/dashboard-page'
-import { WeeklyActivityReport } from '~/components/templates/weekly-activity-report'
 import { retrieveUser } from '~/utils/supabase/queries/profile.query'
 import {
   retrieveMonthlyReport,
-  retrieveWeeklyReport,
   SegmentReportBasedOnDays,
 } from '~/utils/supabase/queries/segments.query'
 import { createClient } from '~/utils/supabase/server-props'
@@ -20,11 +20,11 @@ import 'react-datepicker/dist/react-datepicker.css'
 interface Props {
   date: string,
   user: User,
-  monthlyReport: SegmentReportBasedOnDays
+  report: SegmentReportBasedOnDays
 }
 
 export const getServerSideProps: GetServerSideProps<
-  { user: User; monthlyReport: { day: string; segments: any[] }[] },
+  { user: User; report: { day: string; segments: any[] }[] },
   { date: string }
 > = async (context: GetServerSidePropsContext<{ date: string }>) => {
   const supabase = createClient(context)
@@ -42,18 +42,18 @@ export const getServerSideProps: GetServerSideProps<
   const date = context.params?.date || new Date().toISOString()
   const parsed = new Date(date)
 
-  const monthlyReport = await retrieveMonthlyReport(
+  const report = await retrieveMonthlyReport(
     supabase,
     parsed.getFullYear(),
     parsed.getMonth(),
   )
 
   return {
-    props: { date, user, monthlyReport },
+    props: { date, user, report },
   }
 }
 
-export default function DashboardActivityPage ({ date, monthlyReport }: Props) {
+export default function DashboardActivityPage ({ date, report }: Props) {
   const { t } = useTranslation('dashboard')
 
   const updateDate = useCallback(async (date: Date | null) => {
@@ -63,13 +63,21 @@ export default function DashboardActivityPage ({ date, monthlyReport }: Props) {
 
   return (
     <DashboardPage subtitle={ t('activity.title') }>
-      <DatePicker
-        selected={ new Date(date) }
-        todayButton="Today"
-        onChange={ updateDate }
-        dateFormat="MM/yyyy"
-        showWeekPicker
-      />
+      <Flex sx={ { justifyContent: 'space-between', alignItems: 'center' } }>
+        <h1>Monthly activity report</h1>
+
+        <DatePicker
+          selected={ new Date(date) }
+          todayButton="Today"
+          onChange={ updateDate }
+          dateFormat="MM/yyyy"
+        />
+      </Flex>
+
+      <Card>
+        <Heading as="h3" sx={ { pb: 3 } }>Hourly activity by week day</Heading>
+        <FocusStreakChart report={ report }/>
+      </Card>
     </DashboardPage>
   )
 }
